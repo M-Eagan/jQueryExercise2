e4b1201649defcb3482351578df54e8eeb02f35c
Megan Eagan <Megan.Eagan@gmail.com>
2018-09-10 12:37:58 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+
+
+	jQuery(function($){
+        $('.menu-btn').click(function(){
+        $('.responsive-menu').slideDown()
+        })
+   })
+
+   $( "#numbers" ).click(function() {
+        $('#letters').hide()
+  });
+
+  $(".mobile-nav").click(function() {
+      $('.menu-btn').toggleClass()
+  })
+    
+    });
+
